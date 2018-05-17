f1540c05cca38576d258cb515b8be11d73c3c3d0
Roshini Karnan <grannyismylife@gmail.com>
2018-05-17 14:54:47 +0530

    Updated With Hacker Mode

--- app/src/main/java/com/example/android/infinity/MainActivity.java
+++ app/src/main/java/com/example/android/infinity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.infinity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,10 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     int number;
     Button Color;
     TextView Stone;
+    TextView textView1;
     TextView textView;
     String retrieved;
     int pos;
@@ -28,8 +34,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadData();
+        textView1 = (TextView) findViewById(R.id.stonelist);
         Color= (Button) findViewById(R.id.colorstone);
         Stone= (TextView) findViewById(R.id.namestone);
+        treasure();
         final Button stonegen = (Button) findViewById(R.id.random);
         stonegen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,93 +54,129 @@ public class MainActivity extends AppCompatActivity {
 
     public void stonealloc(int num)
     {
-        if(num==0)
-        {   if(stones.contains("POWER STONE")== FALSE)
-            stones.add("POWER STONE");
+        if(num==0) {
+            if (stones.contains("POWER STONE") == FALSE) {
+                stones.add("POWER STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.purple));
             Stone.setText("POWER STONE");
-            textView=(TextView) findViewById(R.id.power);
-            textView.setText("POWER STONE");
             checkfull();
-        }
-        if(num==1)
+        } else if (num == 1)
         {
-            if(stones.contains("SPACE STONE")== FALSE)
-            stones.add("SPACE STONE");
+            if (stones.contains("SPACE STONE") == FALSE) {
+                stones.add("SPACE STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.blue));
             Stone.setText("SPACE STONE");
-            textView=(TextView) findViewById(R.id.space);
-            textView.setText("SPACE STONE");
             checkfull();
 
-        }
-        if(num==2)
+        } else if (num == 2)
         {
-            if(stones.contains("TIME STONE")== FALSE)
-            stones.add("TIME STONE");
+            if (stones.contains("TIME STONE") == FALSE) {
+                stones.add("TIME STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.green));
             Stone.setText("TIME STONE");
-            textView=(TextView) findViewById(R.id.time);
-            textView.setText("TIME STONE");
             checkfull();
-        }
-        if(num==3)
+        } else if (num == 3)
         {
-            if(stones.contains("REALITY STONE")== FALSE)
-            stones.add("REALITY STONE");
+            if (stones.contains("REALITY STONE") == FALSE) {
+                stones.add("REALITY STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.red));
             Stone.setText("REALITY STONE");
-            textView=(TextView) findViewById(R.id.reality);
-            textView.setText("REALITY STONE");
             checkfull();
 
-        }
-        if(num==4)
+        } else if (num == 4)
         {
-            if(stones.contains("SOUL STONE")== FALSE)
-            stones.add("SOUL STONE");
+            if (stones.contains("SOUL STONE") == FALSE) {
+                stones.add("SOUL STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.orange));
             Stone.setText("SOUL STONE");
-            textView=(TextView) findViewById(R.id.soul);
-            textView.setText("SOUL STONE");
             checkfull();
 
-        }
-        if(num==5)
+        } else if (num == 5)
         {
-            if(stones.contains("MIND STONE")== FALSE)
-            stones.add("MIND STONE");
+            if (stones.contains("MIND STONE") == FALSE) {
+                stones.add("MIND STONE");
+                saveData();
+            }
             Color.setBackgroundColor(getResources().getColor(R.color.yellow));
             Stone.setText("MIND STONE");
-            textView=(TextView) findViewById(R.id.mind);
-            textView.setText("MIND STONE");
             checkfull();
         }
 
     }
-    public void checkfull()
-    {
+
+    public void checkfull() {
         if(stones.contains("MIND STONE")&& stones.contains("SOUL STONE") && stones.contains("REALITY STONE")&&
-                stones.contains("TIME STONE")&& stones.contains("SPACE STONE")&& stones.contains("POWER STONE"))
-        {
+                stones.contains("TIME STONE") && stones.contains("SPACE STONE") && stones.contains("POWER STONE")) {
+            TextView textView1 = (TextView) findViewById(R.id.stonelist);
+            textView1.setText("YOU HAVE NOTHING!");
             stones.clear();
-            textView=(TextView) findViewById(R.id.soul);
-            textView.setText("");
-            textView=(TextView) findViewById(R.id.power);
-            textView.setText("");
-            textView=(TextView) findViewById(R.id.time);
-            textView.setText("");
-            textView=(TextView) findViewById(R.id.reality);
-            textView.setText("");
-            textView.setText("");
-            textView=(TextView) findViewById(R.id.mind);
-            textView.setText("");
-            textView=(TextView) findViewById(R.id.space);
-            textView.setText("");
+            saveData();
             Intent finalIntent = new Intent(MainActivity.this, FinishActivity.class);
             startActivity(finalIntent);
-        }
+            stones.clear();
+        } else
+            treasure();
 
 
     }
+
+    public void reset(View view) {
+        stones.clear();
+        saveData();
+        Color.setBackgroundColor(getResources().getColor(R.color.gray));
+        Stone.setText("");
+        textView1.setText("YOU HAVE NOTHING!");
+
+    }
+
+    public void treasure() {
+        TextView textView1 = (TextView) findViewById(R.id.stonelist);
+        String temp;
+        int n = stones.size();
+        if (n == 0)
+            textView1.setText("YOU HAVE NOTHING!");
+        else {
+            temp = "YOU HAVE WITH YOU: \n";
+            for (int i = 0; i < n; i++)
+                temp += stones.get(i) + "\n";
+            textView1.setText(temp);
+        }
+
+    }
+
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(stones);
+        editor.putString("task list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("task list", null);
+        Type type = new TypeToken<ArrayList<String>>() {
+        }.getType();
+        stones = gson.fromJson(json, type);
+
+    }
+
+
+
+
+
+
 }
