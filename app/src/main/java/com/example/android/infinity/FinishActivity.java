2499b1a061c34f34320a7850b389d4fcccd8467c
Roshini Karnan <grannyismylife@gmail.com>
2018-05-17 14:52:48 +0530

    Updated With Hacker Mode

--- app/src/main/java/com/example/android/infinity/FinishActivity.java
+++ app/src/main/java/com/example/android/infinity/FinishActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FinishActivity extends AppCompatActivity {
 
@@ -12,8 +14,8 @@ public class FinishActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish);
     }
-    public void back (View view)
-    {
+
+    public void back(View view) {
         Intent finalIntent = new Intent(FinishActivity.this, MainActivity.class);
         startActivity(finalIntent);
     }
